cf2ec7e8abda247290ad7e4344cfd7a0285f7ec8
Bhavesh Dewangan <ybkdddd@gmail.com>
2022-10-21 23:05:53 +0530

    add icons for like , comment and share

--- src/components/post/posts.jsx
+++ src/components/post/posts.jsx
@@ -1,6 +1,9 @@
 import { collection, getDocs } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { db } from "../../firebase";
+import comment from "../../images/comment.svg";
+import like from "../../images/postLike.svg";
+import share from "../../images/share.svg";
 
 function Posts() {
   const [imgData, setImgData] = useState([]);
@@ -26,14 +29,27 @@ function Posts() {
 
   useEffect(() => {}, [imgData]);
   return (
-    <div>
+    <div className="flex flex-col gap-28">
       {imgData.map((datas) => {
         return (
-          <div key={datas.id}>
-            <div>
-              <div>{datas.displayName}</div>
-              <img src={datas.displayImage} alt="" />
+          <div key={datas.id} className="flex justify-center">
+            <div className="w-2/6 flex  flex-col border">
+              <div className="flex justify-start gap-10 items-center px-4 py-4">
+                <img
+                  className="w-8 h-8 rounded-full"
+                  src={datas.displayImage}
+                  alt=""
+                />
+                <div className="font-semibold">{datas.displayName}</div>
+              </div>
+
               <img src={datas.imageURL} alt="" />
+              <div className="flex p-4 gap-4">
+                <img src={like} alt="" />
+
+                <img src={comment} alt="" />
+                <img src={share} alt="" />
+              </div>
             </div>
           </div>
         );
